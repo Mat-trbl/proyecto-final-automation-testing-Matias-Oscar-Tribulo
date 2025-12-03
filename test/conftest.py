15243be609015ba913c333cd91259c2c1b0a535f
matias <matias.trbl@gmail.com>
2025-12-03 19:14:40 -0300

    se agrego modificaciones en el conftest.py

--- test/conftest.py
+++ test/conftest.py
@@ -1,5 +1,21 @@
 import pytest
 from utils.helpers import get_driver
+import logging
+import pathlib
+
+path_dir = pathlib.Path('logs')
+path_dir.mkdir(exist_ok=True)
+
+
+logging.basicConfig(
+    filename= path_dir/ "historial.log",
+    level= logging.INFO,
+    format='%(asctime)s %(levelname)s %(name)s - %(message)s',
+    datefmt='%H:%M:%S'
+)
+
+logger = logging.getLogger()
+
 
 @pytest.fixture
 def driver():
